9af39a7ec27c7aece846f59bd44c921491968961
Hadar Koren <hadra.koren.46@gmail.com>
2026-04-29 20:18:35 +0300

    added varient

--- components/GenericButton.jsx
+++ components/GenericButton.jsx
@@ -4,6 +4,7 @@ import './GenericButton.css'
  * Generic reusable button component
  * @param {string} children - Button text
  * @param {string} className - Custom CSS classes for styling/color
+ * @param {string} variant - Button style variant: 'primary', 'secondary', 'danger', 'success', 'info', 'warning', 'outline', 'light'
  * @param {string} size - Button size: 'small', 'medium', 'large'
  * @param {boolean} disabled - Disable button
  * @param {Function} onClick - Click handler
@@ -12,16 +13,19 @@ import './GenericButton.css'
 function GenericButton({
   children,
   className = '',
+  variant = 'secondary',
   size = 'medium',
   disabled = false,
   onClick,
   type = 'button',
   ...props
 }) {
+  const variantClass = variant ? `btn-${variant}` : ''
+  
   return (
     <button
       type={type}
-      className={`generic-btn generic-btn-${size} ${className}`}
+      className={`generic-btn generic-btn-${size} ${variantClass} ${className}`}
       disabled={disabled}
       onClick={onClick}
       {...props}
